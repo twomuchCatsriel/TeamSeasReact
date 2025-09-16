dca8240d5ff747e56330e0b1c61e6424c4158109
StargazerCatsriel <danbro07@imkatta.no>
2025-09-16 10:10:12 +0200

    Start navbar work

---
+++ GoTeamSeas/src/navigationBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav>
+            <div>
+                <ul>
+                    
+                    <li> <img src="./public/teamSeasLogo_Big.png" height={100}></img></li>
+                    <li> <button id="temp"> BAG MATE! </button></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
