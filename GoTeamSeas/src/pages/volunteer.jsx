2ac012dcc941211a80258a26495543e751e92cb6
CatsrielDreemurr <danobrovold@gmail.com>
2025-09-25 10:02:17 +0200

    Add more Volunteer Content
    doing this at my internship lol

--- GoTeamSeas/src/pages/volunteer.jsx
+++ GoTeamSeas/src/pages/volunteer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from "../navigationBar";
 import SetBackgroundImage from "../backgroundImage";
 import ContrastTextbox from "../contrastTextbox";
+import Footerbar from "../footerbar";
+import BasicTextComponent from "../basicTextComponent";
 
 function VolunteerPage(){
     return(
@@ -14,15 +16,22 @@ function VolunteerPage(){
                 headerText="Bli Frivillig i #TeamSeas"
                 infoText="#TeamSeas trenger din hjelp for å kunne gjøre en forskjell. Vi har arrangementer over hele verden, og vi vil gjerne ha deg med på laget."
                 buttonvariant2="orange"
-                button2_text="Bli Frivillig her"
+                button2_text="Meld deg på!"
                 button2_destination="https://youtube.com"
                 
                 ></ContrastTextbox>
             </div>
 
-            <section className="infoSection">
-                <p className="blueTitle">test</p>
+            <section className="infoSection volunteerInfoSection">
+                <BasicTextComponent variant="blue" text="Hvordan blir jeg frivillig?"/>
+                <BasicTextComponent text="For å bli en frivillig, så må du sende inn et innmeldingsskjema. Vi vil ta kontakt med deg så fort vi kan."/> {/* I can not decide if I should make only a register account page, or if I should make a form like RødeKors does. */}
+                <BasicTextComponent text="Når vi tar kontakt, så vil vi dobbeltsjekke med deg at alt i innmeldingsskjemaet er riktig."></BasicTextComponent>
+                <BasicTextComponent text="Hvis alt er riktig, så registerer vi deg som frivillig."></BasicTextComponent>
+                <BasicTextComponent variant="blue" text="Hva gjør en frivillig?"/>
+                <BasicTextComponent text="Våre frivillige jobbed med å rydde opp strender rundt om hele Norge."></BasicTextComponent>
             </section>
+
+            <Footerbar></Footerbar>
         </div>
     )
 }
