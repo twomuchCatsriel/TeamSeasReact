61e066271f495f479966ffea40a9fd20ef5126f6
CatsrielDreemurr <danobrovold@gmail.com>
2025-09-25 10:25:57 +0200

    Finish text i think
    bug found: Contact Info in the Footer Bar  is cut off on mobile for some reason

--- GoTeamSeas/src/pages/volunteer.jsx
+++ GoTeamSeas/src/pages/volunteer.jsx
@@ -24,11 +24,13 @@ function VolunteerPage(){
 
             <section className="infoSection volunteerInfoSection">
                 <BasicTextComponent variant="blue" text="Hvordan blir jeg frivillig?"/>
-                <BasicTextComponent text="For å bli en frivillig, så må du sende inn et innmeldingsskjema. Vi vil ta kontakt med deg så fort vi kan."/> {/* I can not decide if I should make only a register account page, or if I should make a form like RødeKors does. */}
+                <BasicTextComponent text="For å bli en frivillig, så må du sende inn et innmeldingsskjema. Vi vil ta kontakt med deg så fort vi kan."/> {/* I can not decide if I should make only a register account page, or if I should make a form like RødeKors does. Might change in the future*/}
                 <BasicTextComponent text="Når vi tar kontakt, så vil vi dobbeltsjekke med deg at alt i innmeldingsskjemaet er riktig."></BasicTextComponent>
                 <BasicTextComponent text="Hvis alt er riktig, så registerer vi deg som frivillig."></BasicTextComponent>
                 <BasicTextComponent variant="blue" text="Hva gjør en frivillig?"/>
-                <BasicTextComponent text="Våre frivillige jobbed med å rydde opp strender rundt om hele Norge."></BasicTextComponent>
+                <BasicTextComponent text="Våre frivillige jobbed med å rydde opp strender rundt om hele Norge. Som en frivillig, så vil du rydde strender i ditt nærområde."></BasicTextComponent>
+                <BasicTextComponent variant="blue" text="Hvorfor bli frivillig?"></BasicTextComponent>
+                <BasicTextComponent text="Ved å bli frivillig, så har du muligheten til å hjelpe miljøet og nærområdet ditt. ddddddddddddddddddddddddddddddddddddddddddddddddddd"></BasicTextComponent>
             </section>
 
             <Footerbar></Footerbar>
